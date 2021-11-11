058688d0e9737a8a4afd165f430015cf632b89df
Jokermovie2019 <sjpitman22@students.bmchs.org>
2021-11-11 08:37:08 -0600

    hehehe

--- car_game.py
+++ car_game.py
@@ -45,10 +45,10 @@ class Player(pygame.sprite.Sprite):
 
     def update(self):
         pressed_keys = pygame.key.get_pressed()
-       #if pressed_keys[K_UP]:
-            #self.rect.move_ip(0, -5)
-       #if pressed_keys[K_DOWN]:
-            #self.rect.move_ip(0,5)
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -5)
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0,5)
          
         if self.rect.left > 0:
               if pressed_keys[K_LEFT]:
@@ -59,3 +59,21 @@ class Player(pygame.sprite.Sprite):
  
     def draw(self, surface):
         surface.blit(self.image, self.rect)     
+
+P1 = Player()
+E1 = Enemy()
+
+while True:     
+    for event in pygame.event.get():              
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    P1.update()
+    E1.move()
+     
+    DISPLAYSURF.fill(WHITE)
+    P1.draw(DISPLAYSURF)
+    E1.draw(DISPLAYSURF)
+         
+    pygame.display.update()
+    FramesPerSec.tick(FPS)
